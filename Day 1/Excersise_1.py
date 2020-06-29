a911144b49751108aee2aa7ed65662df3c113e20
tadasbytautas <tadasbytautas1@gmail.com>
2020-06-29 15:58:50 +0100

    second commit

---
+++ Day 1/Excersise_1.py
@@ -0,0 +1,5 @@
+number1 = float(input("Please eneter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer: float = number1 + number2
+
+print(number1, "+", number2, "=", answer)
