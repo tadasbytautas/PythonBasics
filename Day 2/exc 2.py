936f09ebcb23f6b0ce93e2d9436ba650ba8e4e02
tadasbytautas <tadasbytautas1@gmail.com>
2020-06-30 11:19:51 +0100

    small changes

--- Day 2/exc 2.py
+++ Day 2/exc 2.py
@@ -2,5 +2,6 @@ word = input("give a word: ")
 vowels = 0
 for char in word:
     if char in "aeiouAEIOU":
+        # if statement could be used with lower() instead of hardcoding capital letters
         vowels = vowels + 1
 print("number of vowels in " + str(word) + " = " + str(vowels))
