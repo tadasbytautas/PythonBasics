54e6fa067594c1f0a0e25297a31473ebde374c82
tadasbytautas <tadasbytautas1@gmail.com>
2020-07-01 11:17:16 +0100

    debugging

---
+++ Day 3/Debugging/exc2.py
@@ -0,0 +1,8 @@
+def product(n):
+    total = 1
+    for n in n:
+        total *= n
+    return total
+
+
+print(product([4, 4, 5]))
---
+++ Day 3/Debugging/exc4.py
@@ -0,0 +1,8 @@
+item_list = ["Burger", "Hotdog", "Bun", "Ketchup", "Cheese"]
+n = 0
+
+while n < 5:
+    for i in item_list:
+        print(item_list)
+
+print(item_list[5])
